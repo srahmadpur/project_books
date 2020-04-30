55d9fb4f54409a1fb3fc60016460fa98af8330b4
Sohrab Ahmadpur <sahmadpur@kibrit.tech>
2020-04-30 20:45:45 +0400

    API logic added

--- application.py
+++ application.py
@@ -1,21 +1,41 @@
-from flask import Flask, session
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import Flask , jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from models import *
 
 app = Flask(__name__)
-
-
-# Configure session to use filesystem
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://ngqctlsfswcnpr:ba7f5dddd401955aaec5b8d0c5a549628479bf6c338efb3ebc77a2d8bd0902ea@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/d3hjdgnvdv8c62'
 app.config["SESSION_PERMANENT"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+db = SQLAlchemy(app)
 
-# Set up database
-engine = create_engine('postgresql://ngqctlsfswcnpr:ba7f5dddd401955aaec5b8d0c5a549628479bf6c338efb3ebc77a2d8bd0902ea@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/d3hjdgnvdv8c62')
-db = scoped_session(sessionmaker(bind=engine))
+
+class Book(db.Model):
+    __tablename__ = "books"
+    isbn = db.Column(db.String, primary_key=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+
 
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template('index.html', title='Home')
+
+
+@app.route("/api/<string:isbn>")
+def isbn_api(isbn):
+    """Return details about a single flight."""
+    # Make sure flight exists.
+    book = Book.query.get(isbn)
+    if book is None:
+        return jsonify({"KeyError": "Invalid ISBN number"}), 422
+    else:    
+        return jsonify({
+                "isbn": book.isbn,
+                "title": book.title,
+                "author": book.author,
+                "year": book.year
+            })
